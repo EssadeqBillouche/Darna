5bbaad929ee9fb01ca9365ca395f275963151d1a
bourraOmar <bwrtmr@gmail.com>
2025-10-24 15:18:16 +0100

    FEAT:DAR-22-Send verification email using Nodemailer.

--- src/presentation/routes/auth.routes.ts
+++ src/presentation/routes/auth.routes.ts
@@ -5,11 +5,14 @@ const router = express.Router();
 import UserRepository from '../../persistence/repositories/UserRepository';
 import { UserService } from '../../business/services/UserService';
 import { UserController } from '../controllers/UserController';
+import { EmailService } from '../../infrastructure/notifications/email.service';
 
 const userRepository = new UserRepository();
-const authService = new UserService(userRepository);
+const emailService = new EmailService();
+const authService = new UserService(userRepository, emailService);
 const authController = new UserController(authService);
 
 router.post('/register', authController.register.bind(authController));
+router.get('/verify-email', authController.verifyEmail.bind(authController));
 
 export default router
