215e7a5f0fced985249d60c7328d505a70764587
bourraOmar <bwrtmr@gmail.com>
2025-10-30 16:31:44 +0100

    FEAT:DAR-33-Add validation with Express-validator

--- src/presentation/routes/biens.routes.ts
+++ src/presentation/routes/biens.routes.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import BienRepository from '../../persistence/repositories/BienRepository';
 import { BienService } from '../../business/services/BienService';
 import { BienController } from '../controllers/BienController';
+import bienValidator from '../middlewares/validators/bien.validation.middleware';
+import { HandleError } from '../middlewares/error.middleware';
 
 const router = express.Router();
 
@@ -11,8 +13,8 @@ const controller = new BienController(service);
 
 router.get('/', controller.list.bind(controller));
 router.get('/:id', controller.getById.bind(controller));
-router.post('/', controller.create.bind(controller));
-router.patch('/:id', controller.update.bind(controller));
+router.post('/', bienValidator.createValidation(), HandleError.validate, controller.create.bind(controller));
+router.patch('/:id', bienValidator.updateValidation(), HandleError.validate, controller.update.bind(controller));
 router.delete('/:id', controller.delete.bind(controller));
 
 export default router;
